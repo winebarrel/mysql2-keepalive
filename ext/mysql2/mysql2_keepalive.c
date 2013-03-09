ed71ba84f0fc46d70cfbf58f44a14da64fcd0967
winebarrel <sgwr_dts@yahoo.co.jp>
2013-03-09 15:09:20 +0000

    add TCP_KEEPCNT setter/getter

--- ext/mysql2/mysql2_keepalive.c
+++ ext/mysql2/mysql2_keepalive.c
@@ -2,10 +2,6 @@
 
 static VALUE rb_eMysql2KeepaliveError;
 
-static VALUE rb_mysql2_client_get(VALUE arg) {
-  VALUE *args = (VALUE *) arg;
-  return rb_const_get(args[0], args[1]);
-}
 
 /* get SO_KEEPALIVE */
 static VALUE rb_mysql2_keepalive_get_keepalive(VALUE self) {
@@ -142,6 +138,54 @@ static VALUE rb_mysql2_client_set_keepintvl(VALUE self, VALUE vOptval) {
   return Qnil;
 }
 
+/* get TCP_KEEPCNT */
+static VALUE rb_mysql2_client_get_keepcnt(VALUE self) {
+#ifdef TCP_KEEPCNT
+  mysql_client_wrapper *wrapper;
+  int sockfd, optval, rv;
+  socklen_t optlen;
+
+  Data_Get_Struct(self, mysql_client_wrapper, wrapper);
+  sockfd = vio_fd(wrapper->client->net.vio);
+
+  optlen = sizeof(optval);
+  rv = getsockopt(sockfd, IPPROTO_TCP, TCP_KEEPCNT, (void*) &optval, &optlen);
+
+  if (rv != 0) { 
+    rb_raise(rb_eMysql2KeepaliveError, "%s", strerror(errno));
+  }
+
+  return INT2NUM(optval);
+#else
+  return Qnil;
+#endif
+}
+
+/* set TCP_KEEPCNT */
+static VALUE rb_mysql2_client_set_keepcnt(VALUE self, VALUE vOptval) {
+#ifdef TCP_KEEPCNT
+  mysql_client_wrapper *wrapper;
+  int sockfd, optval, rv;
+
+  Data_Get_Struct(self, mysql_client_wrapper, wrapper);
+  sockfd = vio_fd(wrapper->client->net.vio);
+  optval = NUM2INT(vOptval);
+
+  rv = setsockopt(sockfd, IPPROTO_TCP, TCP_KEEPCNT, (void*) &optval, sizeof(optval));
+
+  if (rv != 0) { 
+    rb_raise(rb_eMysql2KeepaliveError, "%s", strerror(errno));
+  }
+
+  return Qnil;
+#endif
+}
+
+static VALUE rb_mysql2_client_get(VALUE arg) {
+  VALUE *args = (VALUE *) arg;
+  return rb_const_get(args[0], args[1]);
+}
+
 void Init_keepalive() {
   VALUE rb_cMysql2, rb_cMysql2Client;
   VALUE args[2];
@@ -177,7 +221,10 @@ void Init_keepalive() {
   rb_define_module_function(rb_cMysql2Client, "keepidle=", rb_mysql2_keepalive_set_keepidle, 1);
 
   // TCP_KEEPINTVL
-  rb_define_module_function(rb_cMysql2Client, "keepintvl", rb_mysql2_client_get_keepintvl, 1);
-  rb_define_module_function(rb_cMysql2Client, "keepintvl=", rb_mysql2_client_set_keepintvl, 2);
-}
+  rb_define_module_function(rb_cMysql2Client, "keepintvl", rb_mysql2_client_get_keepintvl, 0);
+  rb_define_module_function(rb_cMysql2Client, "keepintvl=", rb_mysql2_client_set_keepintvl, 1);
 
+  // TCP_KEEPCNT
+  rb_define_module_function(rb_cMysql2Client, "keepcnt", rb_mysql2_client_get_keepcnt, 0);
+  rb_define_module_function(rb_cMysql2Client, "keepcnt=", rb_mysql2_client_set_keepcnt, 1);
+}
